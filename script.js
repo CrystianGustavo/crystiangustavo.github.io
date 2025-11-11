ad58ff4d16d2985d85c0a6e1a82d1e5bc2bd09ec
CrystianGustavo <gustavo.crystian04@gmail.com>
2025-11-11 10:54:25 -0300

    Arrumando typing...

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed(".multiple-text", {
+    strings: [
+        "Teste 1",
+        "Outro teste 2",
+        "Teste 3",
+        "Teste 4"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
